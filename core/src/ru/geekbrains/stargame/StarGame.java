a895859d1d60cab43a0a61921e9d1fb9e54b8bef
prgshut <prgshut@gmail.ru>
2020-04-27 00:23:02 +0300

    HW1

--- core/src/ru/geekbrains/stargame/StarGame.java
+++ core/src/ru/geekbrains/stargame/StarGame.java
@@ -8,19 +8,22 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture img, fon;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		fon=new Texture("fon.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+
+		Gdx.gl.glClearColor(1, 1, 1, 0.1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+		batch.draw(fon,0,0);
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
