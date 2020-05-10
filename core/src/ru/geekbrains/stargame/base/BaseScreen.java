32e0740fec3da70435f4c655372df94f7e63f769
prgshut <prgshut@gmail.ru>
2020-05-10 23:54:03 +0300

    HW3

--- core/src/ru/geekbrains/stargame/base/BaseScreen.java
+++ core/src/ru/geekbrains/stargame/base/BaseScreen.java
@@ -5,13 +5,33 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix3;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.stargame.math.MatrixUtils;
+import ru.geekbrains.stargame.math.Rect;
 
 public class BaseScreen implements Screen, InputProcessor {
+    private Rect screenBounds;
+    private Rect worldBounds;
+    private Rect glBounds;
     protected SpriteBatch batch;
+
+    private Matrix4 worldToGl;
+    private Matrix3 screenToWorld;
+
+    private Vector2 touch;
     @Override
     public void show() {
         Gdx.input.setInputProcessor(this);
         batch = new SpriteBatch();
+        screenBounds = new Rect();
+        worldBounds = new Rect();
+        glBounds = new Rect(0,0, 1f,1f);
+        worldToGl = new Matrix4();
+        screenToWorld = new Matrix3();
+        touch = new Vector2();
     }
 
     @Override
@@ -22,6 +42,19 @@ public class BaseScreen implements Screen, InputProcessor {
 
     @Override
     public void resize(int width, int height) {
+        screenBounds.setSize(width,height);
+        screenBounds.setLeft(0);
+        screenBounds.setBottom(0);
+
+        float aspect = width/(float)height;
+        worldBounds.setHeight(1f);
+        worldBounds.setWidth(1*aspect);
+        MatrixUtils.calcTransitionMatrix(worldToGl, worldBounds, glBounds);
+//        MatrixUtils.calcTransitionMatrix(screenToWorld, screenBounds, worldBounds);
+        batch.setProjectionMatrix(worldToGl);
+        resize(worldBounds);
+    }
+    public void resize(Rect worldBounds){
 
     }
 
@@ -62,16 +95,38 @@ public class BaseScreen implements Screen, InputProcessor {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+        touchDown(touch, pointer, button);
         return false;
     }
 
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        System.out.println("touchDown touch.x = " + touch.x + " touch.y = " + touch.y);
+        return false;
+    }
+
+
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+        touchUp(touch, pointer, button);
+        return false;
+    }
+
+    public boolean touchUp(Vector2 touch, int pointer, int button) {
+        System.out.println("touchUp touch.x = " + touch.x + " touch.y = " + touch.y);
         return false;
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+        touchDragged(touch, pointer);
+        return false;
+    }
+
+    public boolean touchDragged(Vector2 touch, int pointer) {
+        System.out.println("touchDragged touch.x = " + touch.x + " touch.y = " + touch.y);
         return false;
     }
 
--- core/src/ru/geekbrains/stargame/screen/MenuScreen.java
+++ core/src/ru/geekbrains/stargame/screen/MenuScreen.java
@@ -7,29 +7,37 @@ import com.badlogic.gdx.math.Vector2;
 import java.util.Vector;
 
 import ru.geekbrains.stargame.base.BaseScreen;
+import ru.geekbrains.stargame.math.Rect;
+import ru.geekbrains.stargame.sprite.Background;
+import ru.geekbrains.stargame.sprite.Logo;
 
 public class MenuScreen extends BaseScreen {
     private Texture img;
-    private Vector2 pos;
-    private Vector2 drive;
-    private Vector2 direction;
-    private Vector2 touch;
+    private Texture bg;
+    private Background background;
+    private Logo logo;
+
     @Override
     public void show() {
         super.show();
-        img = new Texture("chel.jpg");
-        pos= new Vector2();// наша позиция
-        drive = new Vector2();//движение
-        direction = new Vector2(); //направление
-        touch=new Vector2();// полочаем место тыка
+        img = new Texture("badlogic.jpg");
+        bg = new Texture("textures/bg.png");
+        background = new Background(bg);
+        logo= new Logo(img);
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        background.resize(worldBounds);
+        logo.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        whereMove();
         batch.begin();
-        batch.draw(img,pos.x,pos.y);
+        background.draw(batch);
+        logo.draw(batch);
         batch.end();
     }
 
@@ -37,34 +45,12 @@ public class MenuScreen extends BaseScreen {
     public void dispose() {
         super.dispose();
         img.dispose();
+        bg.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX, Gdx.graphics.getHeight()-screenY);
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        logo.touchDown(touch,pointer,button);
         return false;
-    }
-
-    private void whereMove() {
-        direction.x=touch.x;
-        direction.y=touch.y;
-        direction.sub(pos);
-        direction.nor();
-        drive=direction;// можно изменить скорость движения
-        go();
-    }
-
-    private void go() {
-        pos.add(drive);
-        if ( Math.abs(pos.x-touch.x) <= Math.abs(direction.x)){
-            pos.x=touch.x;
-            drive.x=0;
-        }
-        if ( Math.abs(pos.y-touch.y) <= Math.abs(direction.y)){
-            pos.y=touch.y;
-            drive.y=0;
-        }
-
-
     }
 }
